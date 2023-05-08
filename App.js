149ed290fdc34660d634c2f72928c251fd10f3c6
adrianobzrr <adrianobpessoa@gmail.com>
2023-05-08 14:47:09 -0300

    start of the basket project

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Reactf from 'react';
+import { StatusBar, SafeAreaView,  StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/telas/cesta';
+import { useFonts,  Montserrat_400Regular, Montserrat_700Bold} from "@expo-google-fonts/montserrat";
+
 
 export default function App() {
+  const [fonteCarregada] =useFonts({
+    "MonteserratRegular": Montserrat_400Regular,
+    "MonteserratBold": Montserrat_700Bold,
+  });
+
+  if(!fonteCarregada){
+    return <View/>
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar/>
+      <Cesta/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
